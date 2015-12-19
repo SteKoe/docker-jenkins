65979a7ec053c5ee069efad79781234b7022da70
Stephan Köninger <github@stekoe.de>
2015-12-19 20:49:18 +0100

    chore(periodicBackup): add periodic backup plugin including a folder

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,11 @@
 FROM jenkins
+
+USER root
+RUN mkdir -p /var/jenkins_backup
+
+USER jenkins
+
 ADD plugins.txt /usr/share/jenkins/plugins.txt
 ADD ./init-scripts  /usr/share/jenkins/ref/init.groovy.d/
+
 RUN /usr/local/bin/plugins.sh /usr/share/jenkins/plugins.txt
--- run-jenkins.sh
+++ run-jenkins.sh
@@ -1,5 +1,8 @@
 docker run 	-d \
 			-p 8080:8080 \
-			-v $PWD/jenkins/jenkins_home:/var/jenkins_home \
+            -p 50000:50000 \
+			-v $PWD/jenkins_home:/var/jenkins_home \
+            -v $PWD/jenkins_backup:/var/jenkins_backup \
             --name stekoe_jenkins \
+            --env JAVA_OPTS="-Dhudson.footerURL=http://www.stekoe.de -Xmx2048m" \
             stekoe.de/jenkins
